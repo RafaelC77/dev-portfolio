c939118899908dd7ff9abc6ac4bc87c7d7637d2d
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-11-17 09:44:48 -0300

    feat: create home component

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,44 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import {
+  HomeComponent,
+  HomeInfo,
+  ArrowDown,
+  ProjectsButton,
+  SocialAccounts,
+} from "./styles";
+
+export function Home() {
+  return (
+    <HomeComponent>
+      <HomeInfo>
+        <h1>
+          Olá, me chamo <br /> <strong>Rafael Guimarães.</strong>
+        </h1>
+
+        <h2>
+          Desenvolvedor <b>Front-end</b>
+        </h2>
+
+        <p>
+          Meu foco está em construir aplicações e páginas para web <br /> com
+          design responsivo e acessibilidade.
+        </p>
+
+        <ProjectsButton>Confira meus projetos!</ProjectsButton>
+      </HomeInfo>
+
+      <SocialAccounts>
+        <a href="">
+          <FaLinkedin />
+        </a>
+        <a href="">
+          <FaGithub />
+        </a>
+      </SocialAccounts>
+
+      <a href="#about">
+        <ArrowDown />
+      </a>
+    </HomeComponent>
+  );
+}
---
+++ src/components/Home/styles.ts
@@ -0,0 +1,101 @@
+import styled from "styled-components";
+
+export const HomeComponent = styled.section`
+  height: calc(100vh - 6rem);
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  h1 {
+    font-weight: 400;
+    strong {
+      font-size: 2rem;
+      font-weight: 500;
+      color: ${(props) => props.theme["white"]};
+    }
+  }
+  h2 {
+    font-size: 4rem;
+    font-weight: 400;
+    color: ${(props) => props.theme["green"]};
+  }
+  p {
+    font-size: 1.25rem;
+    color: ${(props) => props.theme["gray"]};
+  }
+  @media (max-width: 480px) {
+    h2 {
+      font-size: 3rem;
+    }
+  }
+`;
+
+export const HomeInfo = styled.div`
+  width: 60%;
+  @media (max-width: 580px) {
+    width: min-content;
+    padding: 1rem;
+  }
+`;
+
+export const SocialAccounts = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  position: absolute;
+  left: 1rem;
+  bottom: 1rem;
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  svg {
+    height: 1.5rem;
+    width: 1.5rem;
+    transition: 0.2s;
+    &:hover {
+      color: ${(props) => props.theme["green"]};
+    }
+  }
+`;
+
+export const ArrowDown = styled.div`
+  height: 2rem;
+  width: 2rem;
+  border-width: 0 3px 3px 0;
+  border-style: solid;
+  border-color: ${(props) => props.theme["green"]};
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  @keyframes arrowMovement {
+    0% {
+      bottom: 2.5rem;
+      opacity: 1;
+    }
+    100% {
+      bottom: 1rem;
+      opacity: 0;
+    }
+  }
+  position: absolute;
+  bottom: 1.5rem;
+  transform: rotate(45deg);
+  animation-name: arrowMovement;
+  animation-duration: 1.5s;
+  animation-iteration-count: infinite;
+`;
+
+export const ProjectsButton = styled.button`
+  background-color: ${(props) => props.theme["green"]};
+  border: none;
+  color: ${(props) => props.theme["black"]};
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  margin-top: 1rem;
+`;
